770bd3dc8b013a47c1381dc6d8bb1df45b7f99db
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-09-29 13:28:15 +0100

    passed

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''Functions to comput pascal traingle'''
+
+
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    res = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            row.append(combination(i, j))
+        res.append(row)
+    return res
+
+
+def fact(n):
+    ''' Takes n and return its fact'''
+    return 1 if n < 2 else n * fact(n - 1)
+
+
+def combination(i, j):
+    ''' equivalent to iCj'''
+    return fact(i) // (fact(j) * fact(i - j))
