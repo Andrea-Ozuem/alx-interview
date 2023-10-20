93bbe9895e3945f33fa24c9c151b70018d64f765
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-20 20:16:08 +0100

    tast 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-'reads stdin line by line and computes metrics'
+"""
+Log parsing
+"""
 
-import re
 import sys
 
 if __name__ == '__main__':
-    count = total = 0
-    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\[.*\])'\
-              r' ("GET /projects/260 HTTP/1.1") (\d{3}) (\d+)$'
+
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
     try:
         for line in sys.stdin:
             count += 1
-            line = line.split()
+            data = line.split()
             try:
-                code = int(line[-2])
-                if code in codes:
-                    codes[code] += 1
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
             except BaseException:
                 pass
             try:
-                total += int(line[-1])
+                filesize += int(data[-1])
             except BaseException:
                 pass
             if count % 10 == 0:
-                print('File size: {:d}'.format(total))
-                for key, val in sorted(codes.items()):
-                    if val > 0:
-                        print('{}: {}'.format(key, val))
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        print('File size: {:d}'.format(total))
-        for key, val in codes.items():
-            if val > 0:
-                print('{}: {}'.format(key, val))
+        print_stats(stats, filesize)
         raise
