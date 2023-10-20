ba6ad0bf37b0a702e726ecb69b6665ee8a2f8d4e
Andrea-Ozuem <andreaozuem2021@gmail.com>
2023-10-20 20:42:35 +0100

    fix: Printing

--- 0x03-log_parsing/0-s.py
+++ 0x03-log_parsing/0-s.py
@@ -4,32 +4,34 @@
 import re
 import sys
 
-count = total = 0
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\[.*\])'\
-          r' ("GET /projects/260 HTTP/1.1") (\d{3}) (\d+)$'
+if __name__ == '__main__':
+    count = total = 0
+    codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\[.*\])'\
+              r' ("GET /projects/260 HTTP/1.1") (\d{3}) (\d+)$'
 
-try:
-    for line in sys.stdin:
-        line = line.rstrip()
-        match = re.fullmatch(pattern, line)
-        if not match:
-            continue
-        code, fs = match.groups()[-2], match.groups()[-1]
-        count += 1
-        total += int(fs)
-        code = int(code)
-        if code not in codes:
-            continue
-        codes[code] += 1
-        if count % 10 == 0:
-            print('File size: {:d}'.format(total))
-            for key, val in sorted(codes.items()):
-                if val > 0:
-                    print('{}: {}'.format(key, val))
-except KeyboardInterrupt:
-    print('File size: {:d}'.format(total))
-    for key, val in codes.items():
-        if val > 0:
-            print('{}: {}'.format(key, val))
-    raise
+    def print_stats(stats: dict, total: int) -> None:
+        '''prints stat'''
+        print("File size: {:d}".format(total))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+    try:
+        for line in sys.stdin:
+            line = line.rstrip()
+            match = re.fullmatch(pattern, line)
+            if not match:
+                continue
+            code, fs = match.groups()[-2], match.groups()[-1]
+            count += 1
+            total += int(fs)
+            code = int(code)
+            if code not in codes:
+                continue
+            codes[code] += 1
+            if count % 10 == 0:
+                print_stats(codes, total)
+        print_stats(codes, total)
+    except KeyboardInterrupt:
+        print_stats(codes, total)
+        raise
--- 0x03-log_parsing/2.py
+++ 0x03-log_parsing/2.py
@@ -1,36 +1,39 @@
 #!/usr/bin/python3
-'reads stdin line by line and computes metrics'
+"""
+Log parsing
+"""
 
-import re
 import sys
 
-count = total = 0
-codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - (\[.*\])'\
-          r' ("GET /projects/260 HTTP/1.1") (\d{3}) (\d+)$'
+if __name__ == '__main__':
 
-try:
-    for line in sys.stdin:
-        count += 1
-        line = line.split()
-        try:
-            code = int(line[-2])
-            if code in codes:
-                codes[code] += 1
-        except BaseException:
-            pass
-        try:
-            total += int(line[-1])
-        except BaseException:
-            pass
-        if count % 10 == 0:
-            print('File size: {:d}'.format(total))
-            for key, val in sorted(codes.items()):
-                if val > 0:
-                    print('{}: {}'.format(key, val))
-except KeyboardInterrupt:
-    print('File size: {:d}'.format(total))
-    for key, val in codes.items():
-        if val > 0:
-            print('{}: {}'.format(key, val))
-    raise
+    total, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
+
+    def print_stats(stats: dict, total: int) -> None:
+        print("File size: {:d}".format(total))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
+
+    try:
+        for line in sys.stdin:
+            count += 1
+            data = line.split()
+            try:
+                code = data[-2]
+                if code in stats:
+                    stats[code] += 1
+            except BaseException:
+                pass
+            try:
+                total += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, total)
+        print_stats(stats, filesize)
+    except KeyboardInterrupt:
+        print_stats(stats, total)
+        raise
